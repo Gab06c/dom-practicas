dbf5d76ef35f1eb7f2b722ff6eddc9fbfd47005c
Gab06c <99303556+Gab06c@users.noreply.github.com>
2022-03-24 18:27:37 -0600

    Html y java

--- contador/app.js
+++ contador/app.js
@@ -1,11 +1,33 @@
 // inicializando nuestro contador
+
 let count = 0
 
 let value = document.getElementById('contador')
-const btnIncrement = document.getElementsByClassName('.increment')
-
 
 function increment() {
     value = count++
     document.getElementById('contador').innerHTML = value
 }
+
+function decrement() {
+    value = count--
+    document.getElementById('contador').innerHTML = value
+}
+
+/* function incDec() {
+    switch (document.getElementsByClassName(.'button-container')){
+        case document.getElementsByClassName('btn increase'):
+            value = count++;
+            document.getElementById('contador').innerHTML = value;
+        break;
+
+        case document.getElementsByClassName('btn decrease'):
+            value = count--;
+            document.getElementById('contador').innerHTML = value;
+        break;
+
+        default:
+            alert('Por favor, da click en un botón');
+        break; 
+    }
+} */
